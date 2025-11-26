fd28eed3144d9ba55a6d5f789f8a2094f836806e
ShirokovaO <sholka.oa@gmail.com>
2025-11-26 20:46:29 +0300

    Practice_9(26.11.2025)_Stream_Callable

---
+++ src/by/java/practice/lesson9_Stream/CarNotFoundException.java
@@ -0,0 +1,7 @@
+package by.java.practice.lesson9_Stream;
+
+public class CarNotFoundException extends RuntimeException {
+    public CarNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/by/java/practice/lesson9_Stream/Multiplication.java
@@ -0,0 +1,5 @@
+package by.java.practice.lesson9_Stream;
+
+public interface Multiplication {
+    Integer multiplication(int a, int b);
+}
